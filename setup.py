7fd10e0d9ed3beaf22d9fc37dc081ca8b6adb1c3
Dean Woods <dmwoods38@gmail.com>
2015-10-15 13:00:23 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='QualysReporting',
+    version='',
+    packages=['qgreports', 'qgreports.config', 'qgreports.scripts'],
+    url='',
+    license='',
+    author='dmwoods38',
+    author_email='',
+    description=''
+)
