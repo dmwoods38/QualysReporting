20eaff4f16bf0d3bb00cf0460477f18ec12dd8ed
Dean Woods <dmwoods38@gmail.com>
2015-12-16 16:59:07 -0500

    Adding support for AWS ES service authentication.

--- qgreports/elasticsearch_connector.py
+++ qgreports/elasticsearch_connector.py
@@ -1,15 +1,16 @@
 import json
 import csv
 import qgreports.objects
-import qgreports.config
+import qgreports.config.settings
 import elasticsearch
+from requests_aws4auth import AWS4Auth
 from qgreports.utils.results_methods import parse_csv_scan_header
 __author__ = 'dmwoods38'
 
 es_mapping_path = os.path.dirname(os.path.realpath(__file__))
 vuln_mapping_path = es_mapping_path + '/config/qualys-vuln-mapping.json'
 scan_mapping_path = es_mapping_path + '/config/qualys-scan-mapping.json'
-es_config = qgreports.config.ELASTICSEARCH
+es_config = qgreports.config.settings.ELASTICSEARCH
 
 
 def initialize_es():
@@ -17,13 +18,19 @@ def initialize_es():
     if es_config['host'] != '':
         if es_config['aws_auth']:
             # Do aws auth stuff here..
-            print 'I can\'t do AWS IAM stuff yet'
+            aws_access_key = qgreports.config.settings.AWS['access_key']
+            aws_secret_key = qgreports.config.settings.AWS['secret_key']
+            aws_region = qgreports.config.settings.AWS['region']
+            auth = AWS4Auth(aws_access_key, aws_secret_key, aws_region, 'es')
+            conn = elasticsearch.connection.RequestsHttpConnection
+            es = elasticsearch.Elasticsearch(host=es_config['host'],
+                                             port=es_config['port'],
+                                             http_auth=auth,
+                                             use_ssl=es_config['use_ssl'],
+                                             connection_class=conn)
         else:
-            if es_config['port'] is not None:
-                es = elasticsearch.Elasticsearch(host=es_config['host'],
-                                                 port=es_config['port'])
-            else:
-                es = elasticsearch.Elasticsearch(host=es_config['host'])
+            es = elasticsearch.Elasticsearch(host=es_config['host'],
+                                             port=es_config['port'])
     else:
         es = elasticsearch.Elasticsearch()
     with open(vuln_mapping_path, 'r') as f:
