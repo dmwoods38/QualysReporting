48b0ff803839111dec4f5cdec538abf030b9d974
Dean Woods <dmwoods38@gmail.com>
2016-01-07 15:09:48 -0500

    filename sanitizing + println debugging

--- qgreports/qualys_connector.py
+++ qgreports/qualys_connector.py
@@ -249,7 +249,7 @@ def get_reports(scheduled_reports, session):
             continue
         params.update({"id": report.report_id})
         report_name = ''.join(c for c in report.email.subject if c.isalnum()
-                              or c in keepcharacters).replace(" ","\ ")
+                              or c in keepcharacters).replace(" ", "\ ")
         report_name = report_name.replace('/', '_') + report_suffix
 
         with open(report_path + report_name, "ab") as f:
@@ -260,8 +260,8 @@ def get_reports(scheduled_reports, session):
         filetype = '.' + report.output
         fullname = report_path + report_name + filetype
 
-        print "breaking here"
         command = "mv " + report_path + report_name + " " + fullname
+        print command
         subprocess.call(command, shell=True)
         report.report_filename = fullname
 
